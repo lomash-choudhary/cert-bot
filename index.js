2915c645cf62e638f87e8a644bc45455a121d8b9
lomash-choudhary <lomashchoudhary9812@gmail.com>
2025-02-20 03:19:25 +0530

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express()
+
+const port = 4000;
+
+app.get("/", (req, res) => {
+    res.status(200).json("Hello from cert-bot https server created by lomash")
+})
+
+
+app.listen(port, () => {
+    console.log("App is listeing on port: ", port)
+})
